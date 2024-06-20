76f1cbcf279aa8700341eb241a432ed9f23679c6
Yesid Martinez <121673430+yesid-martinez@users.noreply.github.com>
2024-06-20 17:50:59 +0000

    Third Copilot Commit

--- comments.js
+++ comments.js
@@ -1,23 +1,17 @@
-// Create a web server
-// Create a web server that listens for incoming requests on port 3000 and responds with the message "Hello, World!".
-// Use the createServer method from the http module to create a new web server.
-// Use the listen method to bind the server to port 3000.
-// Use the writeHead method to set the Content-Type header to text/plain.
-// Use the end method to send the response body "Hello, World!".
-
-// Import the http module
-const http = require('http');
-
-// Create a web server
-const server = http.createServer((req, res) => {
-  // Set the Content-Type header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send the response body
-  res.end('Hello, World!');
-});
-
-// Bind the server to port 3000
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+// Create web server
+// Create a new comment
+// Create a new comment
+router.post('/comments', function(req, res) {
+    // Get the comment data
+    var newComment = req.body;
+    // Create a new comment
+    Comments.create(newComment, function(err, comment) {
+        if (err) {
+            // Error
+            res.json({error: err});
+        } else {
+            // Success
+            res.json(comment);
+        }
+    });
 });
