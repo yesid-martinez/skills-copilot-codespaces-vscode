e3c88f37b72ebf5eb55ad757fb9f9087fc8825cc
Yesid Martinez <121673430+yesid-martinez@users.noreply.github.com>
2024-06-20 17:28:12 +0000

    Update member file

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
 function skillsMember() {
-ber;
+    const member = {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
+        }
+    };
+
+    return member;
 }
